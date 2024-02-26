bd6910d88eb40efb0bed1ae93626c55aab9c79f8
rlaeodus <gwg03185@gmail.com>
2024-02-26 11:50:11 +0900

    Fix : MyPage.jsx - 데이터 띄우기

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,19 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+// import { authCheckToken } from '../apis/auth';
 
+import React from 'react';
 const MyPage = () => {
-  const user = {
-    email: 'gwg03183@naver.com',
-    created_at: Date.now().toString()
-  };
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['loginStatus'],
+    queryFn: authCheckToken
+  });
+  console.log('data', data);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>Error fetching user data</p>
+        <p>Error details: {error.message}</p>
+      </div>
+    );
+  }
+
   return (
-    // <!-- 마이페이지 HTML -->
     <div className='flex h-screen items-center justify-center'>
       <div className='rounded-lg bg-gray-500 p-4'>
         <h1 className='mb-4 text-4xl font-bold'>마이페이지</h1>
-        {user ? (
+        {data && data.user ? (
           <>
-            <p className='mb-2 text-lg'>이메일: {user.email}</p>
-            <p className='text-lg'>가입일: {user.created_at}</p>
+            <p className='mb-2 text-lg'>유저 아이디: {data.user.userId}</p>
+
+            <p className='text-lg'>닉네임: {data.user.nickname}</p>
           </>
         ) : (
           <p className='text-lg'>사용자 정보 없음</p>
@@ -22,5 +39,4 @@ const MyPage = () => {
     </div>
   );
 };
-
 export default MyPage;
