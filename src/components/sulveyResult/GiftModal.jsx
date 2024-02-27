7df45d9cfa97f31ad43600e366247f891e3210a3
HuaHuaChiChi <ksksks102@naver.com>
2024-02-27 18:10:05 +0900

    feat : 추천 상품 댓글 ui 추가

--- src/components/sulveyResult/GiftModal.jsx
+++ src/components/sulveyResult/GiftModal.jsx
@@ -1,4 +1,5 @@
 import gifts from '../../../public/gifts.json';
+import { IoClose } from 'react-icons/io5';
 
 const GiftModal = ({ isModalOpen, setIsModalOpen, selectedGift }) => {
   const gift = gifts.find((item) => item.id === selectedGift);
@@ -12,11 +13,12 @@ const GiftModal = ({ isModalOpen, setIsModalOpen, selectedGift }) => {
           <div className='z-10 flex flex-col items-center rounded-lg bg-white p-6'>
             <div className='mb-4 flex w-full justify-between'>
               <p className='mb-4 text-lg font-bold'>{gift.name}</p>
-              <button
+              <IoClose
+                className=' cursor-pointer'
+                color='#333'
+                size='30px'
                 onClick={() => setIsModalOpen(false)}
-                className='rounded bg-blue-500 px-4 py-2 text-white'>
-                닫기
-              </button>
+              />
             </div>
             <div className='flex gap-6'>
               <img
@@ -24,12 +26,17 @@ const GiftModal = ({ isModalOpen, setIsModalOpen, selectedGift }) => {
                 alt='이미지'
                 className='mb-4 max-h-full max-w-96 rounded'
               />
-              <input
-                className='h-12 w-80 border-gray-300 px-2'
-                placeholder='댓글 입력'></input>
-              <button className='h-12 rounded bg-green-500 px-4 py-2 text-white'>
-                등록
-              </button>
+              <div className='bg-red w-full border-t-2 border-black'>
+                <div className='mb-7 ml-1 flex h-80 border-b-2 border-black'>
+                  <label>aa</label>
+                </div>
+                <input
+                  className='h-12 w-80 border-gray-300 px-2'
+                  placeholder='댓글 입력'></input>
+                <button className='h-12 rounded bg-main px-4 py-2 text-white'>
+                  등록
+                </button>
+              </div>
             </div>
           </div>
         </div>
