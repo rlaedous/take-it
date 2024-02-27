8a94262b92110dc5a9d5c84cba4126f7cc80ee9d
yeriniii <yerini000303@naver.com>
2024-02-27 10:08:10 +0900

    feat:youtube api넣기

--- src/pages/SurveyResultPage.jsx
+++ src/pages/SurveyResultPage.jsx
@@ -108,7 +108,7 @@ const SurveyResultPage = () => {
               src={`https://www.youtube.com/embed/${video.id.videoId}`}
               title={video.snippet.title}
               frameBorder='0'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+              allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
               allowFullScreen></iframe>
             <h3>{video.snippet.title}</h3>
           </div>
