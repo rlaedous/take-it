380526039912455ca744f6e44a721826d8841437
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-02-28 11:42:26 +0900

    style : pagination  페이지 버튼 수정

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,12 +1,24 @@
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   return (
-    <div className='mt-4 flex justify-between'>
+    <div className='mt-4 flex items-center justify-center'>
       {/* 이전 페이지 버튼 */}
       <button
         disabled={currentPage === 1}
         onClick={() => onPageChange(currentPage - 1)}
-        className='cursor-pointer rounded bg-black px-4 py-2 font-bold text-white hover:bg-main hover:text-fuchsia-800'>
-        이전
+        className={`rounded-full p-2 ${currentPage === 1 ? 'cursor-not-allowed text-gray-400' : 'cursor-pointer text-black hover:text-black'}`}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='h-6 w-6'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke='currentColor'>
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth={2}
+            d='M15 19l-7-7 7-7'
+          />
+        </svg>
       </button>
 
       {/* 페이지 번호 버튼들 */}
@@ -18,9 +30,9 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
             disabled={currentPage === index + 1}
             className={`mx-1 ${
               currentPage === index + 1
-                ? 'bg-main text-white'
-                : 'bg-gray-300 text-gray-700'
-            } cursor-pointer rounded px-4 py-2 font-bold hover:bg-main hover:text-fuchsia-800`}>
+                ? 'cursor-not-allowed bg-main text-black hover:bg-main'
+                : 'cursor-pointer text-gray-700 hover:bg-gray-200'
+            } rounded-full px-4 py-2 font-bold transition-colors duration-300`}>
             {index + 1}
           </button>
         ))}
@@ -30,8 +42,20 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       <button
         disabled={currentPage === totalPages}
         onClick={() => onPageChange(currentPage + 1)}
-        className='cursor-pointer rounded bg-black px-4 py-2 font-bold text-white hover:bg-main hover:text-fuchsia-800'>
-        다음
+        className={`rounded-full p-2 ${currentPage === totalPages ? 'cursor-not-allowed text-gray-400' : 'cursor-pointer text-black hover:text-black'}`}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='h-6 w-6'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke='currentColor'>
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth={2}
+            d='M9 5l7 7-7 7'
+          />
+        </svg>
       </button>
     </div>
   );
